5268db2b7b2d29cb5fcf2a305dd417fc6f5a127c
alliehahn <allisonk.hahn@gmail.com>
2018-05-10 00:28:02 -0700

    changed javascript to allow multiple words to be used at random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,70 @@
-var correctLetters = ["b", "l", "a", "n", "k", "e", "t"]
+var words=["blanket", "cat", "yarn", "flower", "tea"]
+var currentWord;
+var guesses=10
+var wins=0
 var correctGuesses = []
 var incorrectGuesses = []
 
-var guesses=10
-var wins=0
+init()
+
+console.log(currentWord);
+
 document.onkeyup = function(event) {
 
-    var userGuess = event.key;
-
-    if (!correctGuesses.includes(userGuess) && correctLetters.includes(userGuess)) {
-        document.getElementById("right").innerHTML+=userGuess + " ";
-        correctGuesses.push(userGuess); 
-    }
-
-    else {
-        if (!incorrectGuesses.includes(userGuess) && !correctLetters.includes(userGuess)) {
-            incorrectGuesses.push(userGuess)
-            console.log(incorrectGuesses)
-            guesses--;
-            document.getElementById("remaining").innerHTML=guesses;
-            document.getElementById("wrong").innerHTML+=userGuess + " ";
+    var userGuess = event.key.toLowerCase();
+        if(currentWord.includes(userGuess)) {
+            if (!correctGuesses.includes(userGuess)){
+                correctGuesses.push(userGuess);
+            }
+        }
+        else {
+            if(!incorrectGuesses.includes(userGuess)) {
+                incorrectGuesses.push(userGuess);
+                guesses--;
+                document.getElementById("remaining").innerHTML=guesses;
+                document.getElementById("wrong").innerHTML+=userGuess + " ";
+                
+            }
             if (guesses==0){
-                reset() 
-            } 
+                reset()
+            }
+            if(currentWord.length==correctGuesses.length) {
+                wins++;
+                
+                reset()
+            }
+            console.log(correctGuesses)
+
         }
 
-    }
-     if (correctGuesses.length==correctLetters.length) {
-         wins++;
-         reset()
-     }
+
+
+        document.getElementById("spaces").innerHTML= ""
+
+        for(var i = 0; i < currentWord.length; i++) {
+            if(correctGuesses.includes(currentWord[i])) {
+                document.getElementById("spaces").innerHTML += currentWord[i] + " "
+            } else {
+                document.getElementById("spaces").innerHTML += "_ ";
+            }
+        }
 
 
 } 
 
 function reset() {
+    init()
     guesses=10
     correctGuesses = []
     incorrectGuesses = []
     document.getElementById("wrong").innerHTML="";
-    document.getElementById("right").innerHTML="";
     document.getElementById("remaining").innerHTML=guesses;
     document.getElementById("wins").innerHTML=wins;
+}
+function init() {
+    //selects random word from array 
+    currentWord=words[Math.floor(Math.random() * words.length)].toLowerCase();
+    for (var i = 0; i < currentWord.length; i++) {
+        document.getElementById ("spaces").innerHTML+= "_ " 
+    }
 }
